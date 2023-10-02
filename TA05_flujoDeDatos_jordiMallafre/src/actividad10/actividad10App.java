9ca84cee664e4554cf9eaf4b802697c8baede244
jordimall <jordi.mallafre@hotmail.com>
2023-10-02 19:27:16 +0200

    Correción nombre variables en la actividad 10

--- TA05_flujoDeDatos_jordiMallafre/src/actividad10/actividad10App.java
+++ TA05_flujoDeDatos_jordiMallafre/src/actividad10/actividad10App.java
@@ -5,14 +5,14 @@ public class actividad10App {
 
 	public static void main(String[] args) {
 		int contador = 1;
-		int num_ventas_A_introducir = 0;
+		int num_ventas_a_introducir = 0;
 		double numero_venta = 0;
 		double numero_ventas_total = 0;
 		String entrada_texto = JOptionPane.showInputDialog("Introduce el numero total de ventas");
 		
-		num_ventas_A_introducir = Integer.parseInt(entrada_texto);
+		num_ventas_a_introducir = Integer.parseInt(entrada_texto);
 		
-		while(contador <= num_ventas_A_introducir) {
+		while(contador <= num_ventas_a_introducir) {
 			entrada_texto = JOptionPane.showInputDialog("Introduce el valor de la venta " + contador);
 			numero_venta = Double.parseDouble(entrada_texto);
 			numero_ventas_total = numero_ventas_total + numero_venta;
